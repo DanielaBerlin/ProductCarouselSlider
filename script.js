9e61ad99d83227d20e6377445a1dd2551de0e39f
Daniela <danielacardona2203@gmail.com>
2023-11-19 21:13:24 +0100

    Implement smooth sliding functionality on 'Next' button click

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-const thumnnails = document.getElementsByClassName
-    ("thumbnail");
-const slider = document.getElementById("slider");
-const nextBtn = document.getElementById("slide-right");
-const prevBtn = document.getElementById("slide-left");
+const thumnnails = document.getElementsByClassName('thumbnail');
+const slider = document.getElementById('slider');
+const nextBtn = document.getElementById('slide-right');
+const prevBtn = document.getElementById('slide-left');
 
-nextBtn.addEventListener("click", () => {
-    scrollAmount = 0;
-    let slideTimer = setInterval(() => {
-        slider.scrollLeft += 10;
-    }, 25);
+nextBtn.addEventListener('click', () => {
+  scrollAmount = 0;
+  let slideTimer = setInterval(() => {
+    slider.scrollLeft += 10;
+    scrollAmount += 10;
+    if ((scrollAmount >= 100)) {
+      window.clearInterval(slideTimer);
+    }
+  }, 25);
+});
+
+nextBtn.addEventListener('click', () => {
+  scrollAmount = 0;
+  let slideTimer = setInterval(() => {
+    slider.scrollLeft -= 10;
+    scrollAmount += 10;
+    if (scrollAmount >= 100) {
+      window.clearInterval(slideTimer);
+    }
+  }, 25);
 });
